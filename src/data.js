18c27bafa090d8b60f50b0774d36076e16a9d014
Amardeep <81394125+Amardeep23@users.noreply.github.com>
2021-10-23 20:31:16 +0530

    data.js created

---
+++ src/data.js
@@ -0,0 +1,109 @@
+import Delhi from './images/delhi.jpg';
+import Mumbai from './images/mumbai.jpg';
+
+export const data =[
+    {
+        id: 1,
+        name: "New Delhi",
+        image: Delhi,
+        tag: "India Gate",
+        pinx: "31.2%",
+        piny: "28.083%",
+        cardx: "31.2%",
+        cardy: "4.083%"  
+    },
+    {
+        id: 2,
+        name: "Mumbai",
+        image: Mumbai,
+        tag: "Gateway of India",
+        pinx: "15.8%",
+        piny: "58.333333%",
+        cardx: "15.8%",
+        cardy: "34.333333%"  
+    },
+    {
+        "id": 3,
+        "name": "City 3",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 4,
+        "name": "City 4",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 5,
+        "name": "City 5",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 6,
+        "name": "City 6",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 7,
+        "name": "City 7",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 8,
+        "name": "City 8",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 9,
+        "name": "City 9",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    },
+    {
+        "id": 10,
+        "name": "City 10",
+        "image": "./Images/.jpg",
+        "tag": "",
+        "pinx": "",
+        "piny": "",
+        "cardx": "",
+        "cardy": ""  
+    }
+    
+   
+]
+
+export default data
